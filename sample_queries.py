b7658bfbe9a5340634e498567f0ad2b5b9138be3
Tyler Sinks <tyler.sinks@gmail.com>
2022-10-24 11:21:21 -0400

    add SQL brainstorming

---
+++ sample_queries.py
@@ -0,0 +1,20 @@
+# write selections in using SLA
+
+
+
+# bring these selections into a working, conntected
+# to a database (even local), Python notebook
+# IDE
+
+
+
+-- for career-level numbers
+-- make a careers table
+
+-- sort for distinct names
+-- -- possibility for different spelling year-on-year?
+
+-- Most points (heighest 25, highest 5)
+
+-- Most net rating change over career
+top 25, top 5
